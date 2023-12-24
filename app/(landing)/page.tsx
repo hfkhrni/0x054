692cf178f3c8782b2d3f3980b6a0f1e331c22f3d
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-24 22:07:46 +0200

    lfeat: lcreate landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start space-y-3 overflow-hidden pt-[12rem]">
+      <h1 className="font-display text-[400px] leading-[350px]">x</h1>
+      <Button className="rounded-sm font-mono text-2xl">
+        TRY 0x54 FREE <ArrowRight className="ml-2" />
+      </Button>
+      <Button variant="secondary" className="rounded-sm font-mono text-base">
+        LOG IN
+      </Button>
+    </main>
+  );
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <h1 className="font-mono text-3xl font-bold underline">Hello world!</h1>
-    </main>
-  );
-}
