cb3f39097740d76714fc23db7533f9705b63e293
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-24 16:24:16 +0200

    chor: configure eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <h1 className="font-mono text-3xl font-bold underline">Hello world!</h1>
     </main>
   );
 }
