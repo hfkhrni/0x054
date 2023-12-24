9dc986378e3a49244af3f497b8afb0dca7bc7813
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-24 23:47:30 +0200

    chore: add use-scroll hook

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,7 +4,7 @@ import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start space-y-3 overflow-hidden pt-[12rem]">
-      <h1 className="font-display text-[400px] leading-[350px]">x</h1>
+      <h1 className="font-display text-[400px] leading-[350px]">X</h1>
       <Button className="rounded-sm font-mono text-2xl">
         TRY 0x54 FREE <ArrowRight className="ml-2" />
       </Button>
