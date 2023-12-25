528c40a65a85cbc1bbce9381b810c286dcc23646
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-25 20:11:55 +0200

    feat: create documents to database from dashboard CTA

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
 import { ModeToggle } from "@/components/mode-toggle";
-import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
+
+import { useUser } from "@clerk/clerk-react";
+
+import { api } from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
 
 function DocumentsPage() {
   const user = useUser();
+  const create = useMutation(api.documents.create);
+
+  function onCreate() {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new file...",
+      success: "file created",
+      error: "Failed to create a file",
+    });
+  }
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-4">
       <p className="font-mono text-sm italic">lonesome empty, expanding void</p>
-      <Button variant="default" className="font-mono text-base italic">
+      <Button variant="default" onClick={onCreate} className="font-mono text-base italic">
         + create a file +
       </Button>
     </div>
