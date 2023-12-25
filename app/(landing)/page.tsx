ed99b93931642f16ecdfb5bccbba4f2c3a265f4a
hfkhrni <hesham.elfakharani@gmail.com>
2023-12-25 02:42:18 +0200

    feat: hook up authentication to landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,17 +1,38 @@
+"use client";
+
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export default function Home() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <main className="flex min-h-screen flex-col items-center justify-start space-y-3 overflow-hidden pt-[12rem]">
       <h1 className="font-display text-[400px] leading-[350px]">X</h1>
-      <Button className="rounded-sm font-mono text-2xl">
-        TRY 0x54 FREE <ArrowRight className="ml-2" />
-      </Button>
-      <Button variant="secondary" className="rounded-sm font-mono text-base">
-        LOG IN
-      </Button>
+      {isLoading && <Spinner />}
+      {!isAuthenticated && !isLoading && (
+        <>
+          <SignInButton mode="modal">
+            <Button className="rounded-sm font-mono text-2xl">
+              TRY 0x54 FREE <ArrowRight className="ml-4" />
+            </Button>
+          </SignInButton>
+        </>
+      )}
+      {isAuthenticated && (
+        <>
+          <Button variant="ghost" className="rounded-sm font-mono text-2xl" asChild={true}>
+            <Link href="/documents">
+              ENTER 0x54 <ArrowRight className="ml-4" />
+            </Link>
+          </Button>
+        </>
+      )}
+
       {/* <ModeToggle></ModeToggle> */}
     </main>
   );
