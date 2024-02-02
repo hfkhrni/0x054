9a2ef72581d771786a9cc84eb8067b0a00546f3b
hfkhrni <hesham.elfakharani@gmail.com>
2024-02-02 19:17:22 +0200

    feat: add search component

---
+++ components/search-command.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useSearch } from "@/hooks/use-search";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { useQuery } from "convex/react";
+import { Asterisk } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export function SearchCommand() {
+  // const { user } = useUser();
+  const router = useRouter();
+  const documents = useQuery(api.documents.getSearch);
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  const toggle = useSearch((store) => store.toggle);
+  const isOpen = useSearch((store) => store.isOpen);
+  const onClose = useSearch((store) => store.onClose);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        toggle();
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, [toggle]);
+
+  function onSelect(id: string) {
+    router.push(`/documents/${id}`);
+    toggle();
+  }
+
+  if (!isMounted) return null;
+
+  return (
+    <CommandDialog
+      open={isOpen}
+      onOpenChange={onClose}
+    >
+      <CommandInput
+        placeholder="Search"
+        className="font-mono placeholder:text-muted-foreground"
+      ></CommandInput>
+      <CommandList>
+        {/* <CommandEmpty className="font-mono text-muted-foreground">
+            NO RESULTS FOUND
+          </CommandEmpty> */}
+        <CommandGroup heading="Documents">
+          {documents?.map((document) => (
+            <CommandItem
+              key={document._id}
+              value={`${document._id}-${document.title}`}
+              onSelect={onSelect}
+            >
+              {document.icon ? (
+                <p className="mr-2 text-[18px]">{document.icon}</p>
+              ) : (
+                <Asterisk className="mr-2 h-4 w-4 text-muted-foreground" />
+              )}
+              <span className="font-mono text-muted-foreground">
+                {document.title}
+              </span>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
+}
