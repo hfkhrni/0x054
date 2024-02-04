6ffa3cb56b16e391fc37ecd0f6d79f470050ba27
hfkhrni <hesham.elfakharani@gmail.com>
2024-02-04 16:44:47 +0200

    feat: add title and toolbar to main doc view

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,35 @@
-function Page() {
-  return <div>Document</div>;
+"use client";
+
+import Toolbar from "@/app/(main)/_components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+type PageProps = {
+  params: {
+    documentId: Id<"documents">;
+  };
+};
+
+function Page({ params }: PageProps) {
+  const id = params.documentId;
+  const document = useQuery(api.documents.getById, { documentId: id });
+
+  if (document === undefined) {
+    return <div>loading</div>;
+  }
+
+  if (document === null) {
+    return <div className="font-mono">Document not found</div>;
+  }
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]"></div>
+      <div className="md:max-w-3xl lg:max-w-4xl">
+        <Toolbar initData={document} />
+      </div>
+    </div>
+  );
 }
 
 export default Page;
---
+++ app/(main)/_components/toolbar.tsx
@@ -0,0 +1,88 @@
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { ImageIcon, Smile, X } from "lucide-react";
+import { useRef, ElementRef, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+
+type ToolbarProps = {
+  initData: Doc<"documents">;
+  preview?: boolean;
+};
+function Toolbar({ initData, preview }: ToolbarProps) {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initData.title);
+  const prevTitle = isEditing ? inputRef.current?.value : "";
+
+  const update = useMutation(api.documents.update);
+  function input() {
+    if (preview) return;
+    setIsEditing(() => true);
+    setValue(initData.title);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current?.value.length);
+    }, 0);
+  }
+
+  function onInput(value: string) {
+    setValue(value);
+    update({ id: initData._id, title: value || "Untitled" });
+  }
+
+  function onKeyDown(event: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+    if (event.key === "Escape") {
+      disableInput();
+    }
+  }
+
+  function disableInput() {
+    setIsEditing(() => false);
+  }
+  return (
+    <div className="group relative pl-[54px]">
+      {!!initData.icon && preview && (
+        <p className="pt-6 text-6xl">{initData.icon}</p>
+      )}
+      <div className="flex items-center gap-x-1 py-4 font-mono opacity-0 group-hover:opacity-100">
+        {!initData.icon && !preview && (
+          <Button
+            variant="outline"
+            size="sm"
+            className="text-xs text-muted-foreground"
+          >
+            <ImageIcon className="mr-2 h-4 w-4" />
+            Add Image
+          </Button>
+        )}
+      </div>
+      {isEditing && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          value={value}
+          onChange={(e) => {
+            onInput(e.target.value);
+          }}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          className="resize-none break-words bg-transparent font-mono text-5xl font-bold text-char-400 outline-none dark:text-neutral-300"
+        ></TextareaAutosize>
+      ) : (
+        <div
+          onClick={input}
+          className="break-words pb-3 font-mono text-5xl font-bold text-char-400 outline-none dark:text-neutral-300"
+        >
+          {initData.title}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Toolbar;
