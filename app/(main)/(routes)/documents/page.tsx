c84c584228273339204dcad4011ba164087996d1
hfkhrni <hesham.elfakharani@gmail.com>
2024-02-06 03:13:00 +0200

    fix: redirect when creating new note from main CTA

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -8,13 +8,16 @@ import { useUser } from "@clerk/clerk-react";
 import { api } from "@/convex/_generated/api";
 import { useMutation } from "convex/react";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 function DocumentsPage() {
-  const user = useUser();
   const create = useMutation(api.documents.create);
+  const router = useRouter();
 
   function onCreate() {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" }).then((documentId) => {
+      router.push(`/documents/${documentId}`);
+    });
 
     toast.promise(promise, {
       loading: "Creating a new file...",
@@ -24,8 +27,14 @@ function DocumentsPage() {
   }
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-4">
-      <p className="font-mono text-sm italic text-muted-foreground">lonesome empty, expanding void</p>
-      <Button variant="default" onClick={onCreate} className="font-mono text-base italic">
+      <p className="font-mono text-sm italic text-muted-foreground">
+        lonesome empty, expanding void
+      </p>
+      <Button
+        variant="default"
+        onClick={onCreate}
+        className="font-mono text-base italic"
+      >
         + create a file +
       </Button>
     </div>
